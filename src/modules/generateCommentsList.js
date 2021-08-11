6c9a332b12189337f4b605fb34c5f66e360cf75a
Konstantin Berezko <berezkok72@gmail.com>
2021-08-11 15:11:01 +0500

    fix(showComment):fix show odd/even style)

--- src/modules/generateCommentsList.js
+++ src/modules/generateCommentsList.js
@@ -2,17 +2,14 @@
 const generateCommentsList = data => {
     const commnetsWrapper = document.querySelector('.commnets-wrapper');
 
-    const list = data.reduce((html, { id, name, email, comment }) => {
-        const evenMark = (id % 2 === 0) ? ' even' : '';
-        return (
+    const list = data.reduce((html, { name, email, comment }) => (
         html + `
             <div class="comment">
-                <div class="comment__name${evenMark}">${name}</div>
-                <div class="comment__email${evenMark}">${email}</div>
+                <div class="comment__name">${name}</div>
+                <div class="comment__email">${email}</div>
                 <div class="comment__text">${comment}</div>
             </div>
-        `);
-    },'')
+        `),'');
     commnetsWrapper.innerHTML = list;
 
 }
